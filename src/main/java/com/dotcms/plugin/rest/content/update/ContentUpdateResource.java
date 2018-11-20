28e2b2d3ad25f81b135ad7ba357bcae92384c458
Will Ezell <will@dotcms.com>
2018-11-20 13:45:29 -0500

    updated for 5.0.3

--- src/main/java/com/dotcms/plugin/rest/content/update/ContentUpdateResource.java
+++ src/main/java/com/dotcms/plugin/rest/content/update/ContentUpdateResource.java
@@ -9,7 +9,6 @@ import javax.servlet.http.HttpServletRequest;
 import com.dotcms.contenttype.model.field.Field;
 import com.dotcms.contenttype.model.type.ContentType;
 import com.dotcms.contenttype.transform.field.LegacyFieldTransformer;
-import com.dotcms.repackage.javax.ws.rs.Consumes;
 import com.dotcms.repackage.javax.ws.rs.GET;
 import com.dotcms.repackage.javax.ws.rs.PUT;
 import com.dotcms.repackage.javax.ws.rs.Path;
@@ -25,7 +24,6 @@ import com.dotmarketing.business.APILocator;
 import com.dotmarketing.business.DotStateException;
 import com.dotmarketing.exception.DotDataException;
 import com.dotmarketing.exception.DotSecurityException;
-import com.dotmarketing.portlets.categories.model.Category;
 import com.dotmarketing.portlets.contentlet.business.ContentletAPI;
 import com.dotmarketing.portlets.contentlet.model.Contentlet;
 import com.dotmarketing.util.json.JSONArray;
@@ -37,100 +35,89 @@ import com.fasterxml.jackson.databind.ObjectWriter;
 import com.google.common.collect.ImmutableMap;
 import com.liferay.portal.model.User;
 
-
-
-
 /**
  * 
  * 
- * Call 
+ * Call
  *
  */
 @Path("/contentUpdate")
-public class ContentUpdateResource  {
+public class ContentUpdateResource {
 
     private final WebResource webResource = new WebResource();
 
+    @Context
+    private HttpServletRequest httpRequest;
 
     @PUT
     @JSONP
     @Path("/update")
-    @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
-    public Response loadJson(@Context HttpServletRequest request,UpdateRequest updateRequest) throws DotStateException,
-            DotDataException, DotSecurityException, JsonProcessingException {
+    public Response loadJson(UpdateRequest updateRequest)
+            throws DotStateException, DotDataException, DotSecurityException, JsonProcessingException {
         // force authentication
-        InitDataObject auth = webResource.init(false, request, false);
+        InitDataObject auth = webResource.init(false, httpRequest, false);
         User user = auth.getUser();
         ContentletAPI conAPI = APILocator.getContentletAPIImpl();
 
-        List<Contentlet> cons = APILocator.getContentletAPIImpl().search(updateRequest.query, 1000, 0,null, user, false);
-        List<Map<String,String>> success = new ArrayList<>();
-        List<Map<String,String>> failures = new ArrayList<>();
-        for(Contentlet con : cons){
-        	
-        	ContentType type=APILocator.getContentTypeAPI(user).find(con.getContentTypeId());
-        	Field field = type.fieldMap().get(updateRequest.field);
-        	
-        	if(field!=null){
-        		Contentlet updateMe = conAPI.checkout(con.getInode(), user, false);
-        		updateMe.getMap().put(Contentlet.DONT_VALIDATE_ME, true);
-        		conAPI.setContentletProperty(updateMe, new LegacyFieldTransformer(field).asOldField(), updateRequest.value);
-        		updateMe = conAPI.checkin(updateMe, user, false);
-        		if(updateRequest.publish){
-        			conAPI.publish(updateMe, user, false);
-        			conAPI.unlock(updateMe, user, false);
-        		}
-        		success.add(ImmutableMap.of("id",updateMe.getIdentifier(),"inode",updateMe.getInode()));
-        	}
-        	else{
-        		failures.add(ImmutableMap.of("id",con.getIdentifier(),"inode",con.getInode()));
-        	}
+        List<Contentlet> cons = APILocator.getContentletAPIImpl().search(updateRequest.query, 1000, 0, null, user, false);
+        List<Map<String, String>> success = new ArrayList<>();
+        List<Map<String, String>> failures = new ArrayList<>();
+        for (Contentlet con : cons) {
+
+            ContentType type = APILocator.getContentTypeAPI(user).find(con.getContentTypeId());
+            Field field = type.fieldMap().get(updateRequest.field);
+
+            if (field != null) {
+                Contentlet updateMe = conAPI.checkout(con.getInode(), user, false);
+                updateMe.getMap().put(Contentlet.DONT_VALIDATE_ME, true);
+                conAPI.setContentletProperty(updateMe, new LegacyFieldTransformer(field).asOldField(), updateRequest.value);
+                updateMe = conAPI.checkin(updateMe, user, false);
+                if (updateRequest.publish) {
+                    conAPI.publish(updateMe, user, false);
+                    conAPI.unlock(updateMe, user, false);
+                }
+                success.add(ImmutableMap.of("id", updateMe.getIdentifier(), "inode", updateMe.getInode()));
+            } else {
+                failures.add(ImmutableMap.of("id", con.getIdentifier(), "inode", con.getInode()));
+            }
         }
         JSONObject jo = new JSONObject();
         try {
-			jo.put("success", new JSONArray(success));
-			jo.put("failures", new JSONArray(failures));
-		} catch (JSONException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-        
+            jo.put("success", new JSONArray(success));
+            jo.put("failures", new JSONArray(failures));
+        } catch (JSONException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
 
         ResponseBuilder builder = Response.ok(jo.toString(), MediaType.APPLICATION_JSON);
 
         builder.header("Access-Control-Expose-Headers", "Authorization");
         builder.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-        
+
         return builder.build();
 
     }
-  
+
     @GET
     @JSONP
     @Path("/test")
     @Produces(MediaType.APPLICATION_JSON)
-    public Response test(@Context HttpServletRequest request) throws DotStateException,
-            DotDataException, DotSecurityException, JsonProcessingException {
+    public Response test() throws DotStateException, DotDataException, DotSecurityException, JsonProcessingException {
         // force authentication
-        InitDataObject auth = webResource.init(false, request, false);
-        User user = auth.getUser();
-        
+        InitDataObject auth = webResource.init(false, httpRequest, false);
 
-        
         ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
 
-        
-        
         String json = ow.writeValueAsString("ok");
         ResponseBuilder builder = Response.ok(json, MediaType.APPLICATION_JSON);
 
         builder.header("Access-Control-Expose-Headers", "Authorization");
         builder.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-        
+
         return builder.build();
 
     }
-  
 
 }
